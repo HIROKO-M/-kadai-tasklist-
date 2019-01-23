308b083db66917e240bec10467ba9768bd8029fc
hiroko.m <hiroko_m@test.jp>
2019-01-23 02:28:44 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Task Lists</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
-        </div>
-    </div>
+    @if (Auth::check())
+        <?php $user = Auth::user();   // ユーザがログインしていれば、現在認証されているユーザーの取得
+        ?>
+        {{ $user->name }}
 
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Task Lists</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    
+    @endif
+    
 @endsection
